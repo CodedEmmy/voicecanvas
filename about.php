7e5cc1e5b794f22d8dbf8607a0cf1e14563fe532
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2024-11-21 14:18:59 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>VoiceCanvas - About</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">-->
+	
+	<!-- Favicons -->
+	<link href="images/favicon.png" rel="icon">
+	
+	<link rel="stylesheet" href="css/voicecanvas.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/owl.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** 
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-md-10">
+          <ul class="info">
+            <li><i class="fa fa-microphone"></i> Voice Only</li>
+            <li><i class="fa fa-image"></i> AI Imagery</li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-2">
+          <ul class="social-links">
+            <li><a href="#"><i class="fa fa-paint-brush"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+	<header class="header-area header-sticky">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<nav class="main-nav">
+						<!-- ***** Logo Start ***** -->
+						<a href="index.php" class="logo">
+							<img src="images/logo.png">
+						</a>
+						<!-- ***** Logo End ***** -->
+						<ul class="nav">
+						  <li><a href="index.php">Home</a></li>
+						  <li><a href="about.php" class="active">About</a></li>
+						  <li>&nbsp;</li>
+					  </ul>   
+						<a class='menu-trigger'>
+							<span>Menu</span>
+						</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div class="page-heading header-text">
+		<div class="container">
+		  <div class="row">
+			<div class="col-lg-12">
+			  <h3>About</h3>
+			</div>
+		  </div>
+		</div>
+	</div>
+	
+	<div class="single-property section">
+		<div class="container">
+		  <div class="row">
+			<div class="col-lg-12">
+			  <div class="main-content">
+				<!--<h4>About Voice Canvas</h4>-->
+				<p>About Voice Canvas text
+				
+				</p>
+				<hr>
+				<p id="ic">
+				Image Credits:<br>
+				
+				</p>
+			  </div> 
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-8">
+        <p>VoiceCanvas </p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="libs/jquery/jquery.min.js"></script>
+  <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+  <script src="js/isotope.min.js"></script>
+  <script src="js/owl-carousel.js"></script>
+  <script src="js/counter.js"></script>
+  <script src="js/custom.js"></script>
+
+  </body>
+</html>
---
+++ canvas.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>VoiceCanvas</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">-->
+	
+	<!-- Favicons -->
+	<link href="images/favicon.png" rel="icon">
+
+    <!-- Bootstrap core CSS -->
+    <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/voicecanvas.css">
+    <link rel="stylesheet" href="css/owl.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** 
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-md-10">
+          <ul class="info">
+            <li><i class="fa fa-microphone"></i> Voice Only</li>
+            <li><i class="fa fa-image"></i> AI Imagery</li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-2">
+          <ul class="social-links">
+            <li><a href="#"><i class="fa fa-paint-brush"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+						<img src="images/logo.png">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <ul class="nav">
+                      <li><a href="index.php" class="active">Home</a></li>
+                      <li><a href="about.php">About</a></li>
+                      <li>&nbsp;</li>
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+	<div id="fb_box">check</div>
+	
+	<div class="page-heading header-text">
+		<div class="container">
+		  <div class="row">
+			<div class="col-lg-12">
+			  <h3>Generated Image</h3>
+			</div>
+		  </div>
+		</div>
+	</div>
+	
+	<div class="single-property section">
+		<div class="container">
+		  <div class="row">
+			<div class="col-lg-8">
+			  <div class="main-image">
+				<img src="images/default.jpg" alt="">
+			  </div>
+			  <div class="main-content">
+				<h4>Input Prompt</h4>
+				<p>Prompt text</p>
+			  </div> 
+			</div>
+			<div class="col-lg-4">
+			  <div class="info-table">
+				<ul>
+				  <li>
+					<img src="images/marker.png" alt="" style="max-width: 52px;">
+					<h4>Image/JPEG<br><span>Image Type</span></h4>
+				  </li>
+				  <li>
+					<img src="images/marker.png" alt="" style="max-width: 52px;">
+					<h4>800px<br><span>Width</span></h4>
+				  </li>
+				  <li>
+					<img src="images/marker.png" alt="" style="max-width: 52px;">
+					<h4>350px<br><span>Height</span></h4>
+				  </li>
+				  <li>
+					<img src="images/marker.png" alt="" style="max-width: 52px;">
+					<h4>Download<br><span>Download link</span></h4>
+				  </li>
+				</ul>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-8">
+        <p>VoiceCanvas </p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="libs/jquery/jquery.min.js"></script>
+  <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+  <script src="js/isotope.min.js"></script>
+  <script src="js/owl-carousel.js"></script>
+  <script src="js/counter.js"></script>
+  <script src="js/custom.js"></script>
+
+  </body>
+</html>
