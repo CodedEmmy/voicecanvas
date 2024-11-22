a0c39ef0fa063820a081710ec5034dee57caa03d
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2024-11-22 02:08:39 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,21 +3,4 @@
 $API_KEY = "";
 $AI_ENDPOINT = "https://api.limewire.com/api/image/generation";
 
-//Some utility functions
-function file_get_contents_curl($url)
-{
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	$cdata = curl_exec($ch);
-	curl_close($ch);
-	return $cdata;
-}
-
-function copyImageToServer($dest, $src)
-{
-	$srcImg = file_get_contents_curl($src);
-	file_put_contents($dest, $srcImg);
-}
 ?>
